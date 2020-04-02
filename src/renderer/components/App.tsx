9dcfc381967bf7c9bfa721ad3b80e0bb6ea8527a
arindamdawn <arindamdawn3@gmail.com>
2020-04-02 22:41:11 +0530

    updated config

---
+++ src/renderer/components/App.tsx
@@ -0,0 +1,10 @@
+import { hot } from "react-hot-loader/root";
+import React from "react";
+
+export default hot(
+  (): JSX.Element => (
+    <div>
+      <h1>My first electron App is now setup!</h1>
+    </div>
+  )
+);
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,17 +1,25 @@
-const rules = require('./webpack.rules');
-const plugins = require('./webpack.plugins');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
+const rules = require("./webpack.rules");
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+function srcPaths(src) {
+  return path.join(__dirname, src);
+}
 
 module.exports = {
+  mode: "development",
+  target: "electron-renderer",
+  devtool: "source-map",
   module: {
-    rules,
+    rules
   },
-  plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
-  },
+    alias: {
+      "@main": srcPaths("src/main"),
+      "@models": srcPaths("src/models"),
+      "@renderer": srcPaths("src/renderer"),
+      "react-dom": "@hot-loader/react-dom"
+    },
+    extensions: [".js", ".ts", ".tsx", ".jsx", ".json"]
+  }
 };
